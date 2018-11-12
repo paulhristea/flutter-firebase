2263b72d8bce6401e4765513f97ad953558ad190
Paul Hristea <hristea.paul.petrisor@gmail.com>
2018-11-12 20:31:54 +0200

    Implemented Splash Screen for Android

--- android/app/src/main/kotlin/com/freelancingpeople/flutterfirebase/MainActivity.kt
+++ android/app/src/main/kotlin/com/freelancingpeople/flutterfirebase/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.freelancingpeople.flutterfirebase
 
 import android.os.Bundle
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity(): FlutterActivity() {
+class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    val initialColor = window.statusBarColor
+    window.statusBarColor = 0x00000000
+
     GeneratedPluginRegistrant.registerWith(this)
+
+    val layoutListener = object : ViewTreeObserver.OnGlobalLayoutListener {
+      override fun onGlobalLayout() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.statusBarColor = initialColor
+        flutterView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+      }
+    }
+    flutterView.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
   }
 }
